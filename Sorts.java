de74036eb2d3077c85516ea63165c66e3f16a226
Doll <19jdoll@wsdweb.org>
2017-10-29 19:59:41 -0400

    Added more sorts
    These sorts are untested, but should be useful come next game

--- Sorts.java
+++ Sorts.java
@@ -103,4 +103,156 @@ public class Sorts
          	teams.set( max, temp );
       	}
    	}
+	
+	public static void sortByTeleOpAverageScoreDown ( ArrayList<Team> teams )
+   	{
+      	int min;
+      	Team temp;
+
+      	for ( int index = 0; index < teams.size() - 1; index++ )
+      	{
+         	min = index;
+         	for ( int scan = index + 1; scan < teams.size(); scan++ )
+            	if ( teams.get(scan).getTeleOpAverageScore() < teams.get(min).getTeleOpAverageScore() )
+               		min = scan;
+
+         	// Swap the values
+         	temp = teams.get(min);
+         	teams.set( min, teams.get(index) );
+         	teams.set( min, temp );
+      	}
+   	}
+	
+	public static void sortByTeleOpAverageScoreUp ( ArrayList<Team> teams )
+   	{
+      	int max;
+      	Team temp;
+
+      	for ( int index = teams.size() - 1; index > 0; index-- )
+      	{
+         	max = index;
+         	for ( int scan = index - 1; scan > 0; scan-- )
+            	if ( teams.get(scan).getTeleOpAverageScore() > teams.get(max).getTeleOpAverageScore() )
+               		max = scan;
+
+         	// Swap the values
+         	temp = teams.get(max);
+         	teams.set( max, teams.get(index) );
+         	teams.set( max, temp );
+      	}
+   	}
+
+public static void sortByTeleOpTotalScoreDown ( ArrayList<Team> teams )
+	{
+  	int min;
+  	Team temp;
+
+  	for ( int index = 0; index < teams.size() - 1; index++ )
+  	{
+     	min = index;
+     	for ( int scan = index + 1; scan < teams.size(); scan++ )
+        	if ( teams.get(scan).getTeleOpTotalScore() < teams.get(min).getTeleOpTotalScore() )
+           		min = scan;
+
+     	// Swap the values
+     	temp = teams.get(min);
+     	teams.set( min, teams.get(index) );
+     	teams.set( min, temp );
+  	}
+	}
+
+	public static void sortByTeleOpTotalScoreUp ( ArrayList<Team> teams )
+	{
+	  	int max;
+	  	Team temp;
+	
+	  	for ( int index = teams.size() - 1; index > 0; index-- )
+	  	{
+	     	max = index;
+	     	for ( int scan = index - 1; scan > 0; scan-- )
+	        	if ( teams.get(scan).getTeleOpTotalScore() > teams.get(max).getTeleOpTotalScore() )
+	           		max = scan;
+	
+	     	// Swap the values
+	     	temp = teams.get(max);
+	     	teams.set( max, teams.get(index) );
+	     	teams.set( max, temp );
+	  	}
+	}
+	
+	public static void sortByAutoTotalScoreDown ( ArrayList<Team> teams )
+   	{
+      	int min;
+      	Team temp;
+
+      	for ( int index = 0; index < teams.size() - 1; index++ )
+      	{
+         	min = index;
+         	for ( int scan = index + 1; scan < teams.size(); scan++ )
+            	if ( teams.get(scan).getAutoTotalScore() < teams.get(min).getAutoTotalScore() )
+               		min = scan;
+
+         	// Swap the values
+         	temp = teams.get(min);
+         	teams.set( min, teams.get(index) );
+         	teams.set( min, temp );
+      	}
+   	}
+	
+	public static void sortByAutoTotalScoreUp ( ArrayList<Team> teams )
+   	{
+      	int max;
+      	Team temp;
+
+      	for ( int index = teams.size() - 1; index > 0; index-- )
+      	{
+         	max = index;
+         	for ( int scan = index - 1; scan > 0; scan-- )
+            	if ( teams.get(scan).getAutoTotalScore() > teams.get(max).getAutoTotalScore() )
+               		max = scan;
+
+         	// Swap the values
+         	temp = teams.get(max);
+         	teams.set( max, teams.get(index) );
+         	teams.set( max, temp );
+      	}
+   	}
+	
+	public static void sortByTeamNumDown ( ArrayList<Team> teams )
+   	{
+      	int min;
+      	Team temp;
+
+      	for ( int index = 0; index < teams.size() - 1; index++ )
+      	{
+         	min = index;
+         	for ( int scan = index + 1; scan < teams.size(); scan++ )
+            	if ( teams.get(scan).getTeamNum() < teams.get(min).getTeamNum() )
+               		min = scan;
+
+         	// Swap the values
+         	temp = teams.get(min);
+         	teams.set( min, teams.get(index) );
+         	teams.set( min, temp );
+      	}
+   	}
+	
+	public static void sortByTeamNumUp( ArrayList<Team> teams )
+   	{
+      	int max;
+      	Team temp;
+
+      	for ( int index = teams.size() - 1; index > 0; index-- )
+      	{
+         	max = index;
+         	for ( int scan = index - 1; scan > 0; scan-- )
+            	if ( teams.get(scan).getTeamNum() > teams.get(max).getTeamNum() )
+               		max = scan;
+
+         	// Swap the values
+         	temp = teams.get(max);
+         	teams.set( max, teams.get(index) );
+         	teams.set( max, temp );
+      	}
+   	}
 }
