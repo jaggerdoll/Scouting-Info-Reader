468f84a0b0d5962cdebafd2c6850d1bb8a3ef374
Doll <19jdoll@wsdweb.org>
2017-10-29 20:34:41 -0400

    @19cstevens tried to make your life easier
    Moved around some methods to make it proper, added some intializations and a basic predictWinner method in the Match class

--- Event.java
+++ Event.java
@@ -1,40 +1,53 @@
+import java.util.ArrayList;
+
 public class Event
 {
-	private Match[] matchList;
-	private Team[] allAttendingTeams;
-	private Team[] predictedRankings;
+	private ArrayList<Match> matchList;
+	private ArrayList<Team> allAttendingTeams;
+	private ArrayList<Team> predictedRankings;
 	private Team[] winners;
+	
 	public Event()
 	{
-	
+		allAttendingTeams = new ArrayList<Team>();
+		predictedRankings = new ArrayList<Team>();
+		matchList = new ArrayList<Match>();
+		winners = new Team[3];
+	}
+	public ArrayList<Match> getMatchList()
+	{
+		return matchList;
+	}
+	public void setMatchList(ArrayList<Match> matchList)
+	{
+		this.matchList = matchList;
+	}
+	public ArrayList<Team> getAllAttendingTeams()
+	{
+		return allAttendingTeams;
+	}
+	public void setAllAttendingTeams(ArrayList<Team> allAttendingTeams)
+	{
+		this.allAttendingTeams = allAttendingTeams;
+	}
+	public ArrayList<Team> getPredictedRankings()
+	{
+		return predictedRankings;
+	}
+	public void setPredictedRankings(ArrayList<Team> predictedRankings)
+	{
+		this.predictedRankings = predictedRankings;
+	}
+	public Team[] getWinners()
+	{
+		return winners;
+	}
+	public void setWinners(Team[] winners)
+	{
+		this.winners = winners;
 	}
 	public void predictRankings()
 	{
 		
 	}
-	public Match[] getMatchList() {
-		return matchList;
-	}
-	public void setMatchList(Match[] matchList) {
-		this.matchList = matchList;
-	}
-	public Team[] getAllAttendingTeams() {
-		return allAttendingTeams;
-	}
-	public void setAllAttendingTeams(Team[] allAttendingTeams) {
-		this.allAttendingTeams = allAttendingTeams;
-	}
-	public Team[] getPredictedRankings() {
-		return predictedRankings;
-	}
-	public void setPredictedRankings(Team[] predictedRankings) {
-		this.predictedRankings = predictedRankings;
-	}
-	public Team[] getWinners() {
-		return winners;
-	}
-	public void setWinners(Team[] winners) {
-		this.winners = winners;
-	}
-	
 }
--- Match.java
+++ Match.java
@@ -6,49 +6,78 @@ public class Match
 	private int redTeamScore;
 	private String winningAlliance;
 	private String predictedWinningAlliance;
+	
 	public Match()
 	{
-  
-	}
-	public void predictWinner()
-	{
+		redAlliance = new Team[3];
+		blueAlliance = new Team[3];
 		
+		redTeamScore = 0;
+		blueTeamScore = 0;
+		
+		winningAlliance = "Rue";
+		predictedWinningAlliance = "Rue";
 	}
-	public Team[] getRedAlliance() {
+	public Team[] getRedAlliance()
+	{
 		return redAlliance;
 	}
-	public void setRedAlliance(Team[] redAlliance) {
+	public void setRedAlliance(Team[] redAlliance)
+	{
 		this.redAlliance = redAlliance;
 	}
-	public Team[] getBlueAlliance() {
+	public Team[] getBlueAlliance()
+	{
 		return blueAlliance;
 	}
-	public void setBlueAlliance(Team[] blueAlliance) {
+	public void setBlueAlliance(Team[] blueAlliance)
+	{
 		this.blueAlliance = blueAlliance;
 	}
-	public int getBlueTeamScore() {
+	public int getBlueTeamScore()
+	{
 		return blueTeamScore;
 	}
-	public void setBlueTeamScore(int blueTeamScore) {
+	public void setBlueTeamScore(int blueTeamScore)
+	{
 		this.blueTeamScore = blueTeamScore;
 	}
-	public int getRedTeamScore() {
+	public int getRedTeamScore()
+	{
 		return redTeamScore;
 	}
-	public void setRedTeamScore(int redTeamScore) {
+	public void setRedTeamScore(int redTeamScore)
+	{
 		this.redTeamScore = redTeamScore;
 	}
-	public String getWinningAlliance() {
+	public String getWinningAlliance()
+	{
 		return winningAlliance;
 	}
-	public void setWinningAlliance(String winningAlliance) {
+	public void setWinningAlliance(String winningAlliance)
+	{
 		this.winningAlliance = winningAlliance;
 	}
-	public String getPredictedWinningAlliance() {
+	public String getPredictedWinningAlliance()
+	{
 		return predictedWinningAlliance;
 	}
-	public void setPredictedWinningAlliance(String predictedWinningAlliance) {
+	public void setPredictedWinningAlliance(String predictedWinningAlliance)
+	{
 		this.predictedWinningAlliance = predictedWinningAlliance;
 	}
 	
+	public void readSchedule()
+	{
+		
+	}
+	
+	public void predictWinner()
+	{
+		if( redAlliance[0].getAverageScore() + redAlliance[1].getAverageScore() + redAlliance[2].getAverageScore()
+				< blueAlliance[0].getAverageScore() + blueAlliance[1].getAverageScore() + blueAlliance[2].getAverageScore() )
+			predictedWinningAlliance = "BLUE";
+		else
+			predictedWinningAlliance = "RED";
+	}
 }
