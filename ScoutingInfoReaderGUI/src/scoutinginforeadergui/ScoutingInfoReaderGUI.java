404c35d9fd2f0505ec84ea58a39207803a7c9609
Doll <19jdoll@wsdweb.org>
2017-10-30 08:21:35 -0400

    Added NetBeans Files to create the GUi for the App
    DO NOT TOUCH THESE. I WILL DO THIS @Everyone

---
+++ ScoutingInfoReaderGUI/src/scoutinginforeadergui/ScoutingInfoReaderGUI.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package scoutinginforeadergui;
+
+/**
+ *
+ * @author 19jdoll
+ */
+public class ScoutingInfoReaderGUI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
